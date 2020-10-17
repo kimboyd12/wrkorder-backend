074b031c207bbe8541802fd3b50cf6a2e001e4fb
kim-boyd <kimberlyboyd2@gmail.com>
2020-10-16 20:11:31 -0400

    starting

---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+const session = require("express-session")
+
+
+const welcomeRouter = require("./welcome-router")
+
+
+const server = express()
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+server.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.JWT_SECRET,
+}))
+
+
+server.use("/", welcomeRouter)
+
+
+server.use((err, req, res, next) => {
+    console.log(err)
+
+    res.status(500).json({
+        message: "Something went wrong, please try again",
+    })
+})
+
+
+
+module.exports = server
